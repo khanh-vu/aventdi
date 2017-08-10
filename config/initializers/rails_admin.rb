e4be23e5fd70f0c9f4bd2eed864c878c414cb3fe
Khanh Vu <khanh.vu@8bitrockr.com>
2017-08-10 17:54:00 +0700

    Show model "Business address"

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -36,7 +36,7 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
-  config.excluded_models = ["BusinessEventAddress", "BusinessAddress", "EventActivity"]
+  config.excluded_models = ["BusinessEventAddress", "EventActivity"]
 end
 
 RailsAdmin.config do |config|
