491f7079b8274b89500362b995ac028b30f70907
Khanh Vu <khanh.vu@8bitrockr.com>
2017-08-10 17:41:39 +0700

    Hide models "Business Address" and "Event Activity"

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -36,7 +36,7 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
-  config.excluded_models = ["BusinessEventAddress"]
+  config.excluded_models = ["BusinessEventAddress", "BusinessAddress", "EventActivity"]
 end
 
 RailsAdmin.config do |config|
